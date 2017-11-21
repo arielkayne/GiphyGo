37839abcab34915af3e692ae9a95b40ce36d856f
akayne <15270231+arielkayne@users.noreply.github.com>
2017-11-20 23:00:09 -0600

    comments added

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -86,6 +86,14 @@ $("button").on("click", function() {
     });
 });
 
+
+// 5 (done above ...search var p
+// 	  Under every gif, display its rating (PG, G, so on). 
+//    * This data is provided by the GIPHY API.
+//    * Only once you get images displaying with button presses should you move on to the next step.
+// 6. Add a form to your page takes the value from a user input box and adds it into your `topics` array. 
+// Then make a function call that takes each topic in the array remakes the buttons on the page.
+
     	$("#addItem").on("click", function(event) {
 
 	        // event.preventDefault() can be used to prevent an event's default behavior.
@@ -108,9 +116,4 @@ $("button").on("click", function() {
 	    	}
 		});
 }
-
-
-// 6. Add a form to your page takes the value from a user input box and adds it into your `topics` array. 
-// Then make a function call that takes each topic in the array remakes the buttons on the page.
-
 
